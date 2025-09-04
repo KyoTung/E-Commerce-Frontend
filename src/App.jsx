6f96cc7c09f48886b6d02bfa1221d01c6ead8177
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-09-04 22:47:10 +0700

    feat: tao cau truc thu muc, ket noi tailwind, cau hinh chung

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="text-center p-8 bg-gray-100 min-h-screen bg-red-600">
+      <h1 className="text-3xl font-bold text-green-600">
+        Tailwind CSS hoạt động!
+      </h1>
+      <p className="text-gray-700 mt-4">
+        Phiên bản ổn định: Tailwind 3.4 + Vite + React
+      </p>
+    </div>
+  );
+}
