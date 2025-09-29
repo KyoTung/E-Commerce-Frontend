ddd1f9cf5db5a52a507c770ac2f5fab1d2b89261
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-09-29 23:09:36 +0700

    feat: cap nhat authSlice login

--- src/features/authSlice/authService.jsx
+++ src/features/authSlice/authService.jsx
@@ -2,5 +2,12 @@ const baseURL = import.meta.env.VITE_BASE_URL;
 import axios from "axios";
 
 const login = async(userData) =>{
-    const reponse = await axios.post(`${baseURL}/user/login`, userData)
+    const reponse = await axios.post(`${baseURL}/user/login`, userData);
+    console.log(reponse)
 }
+
+const authService = {
+    login,
+}
+
+export default authService;
--- src/features/authSlice/authSlice.jsx
+++ src/features/authSlice/authSlice.jsx
@@ -1,7 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 const userState = {
-  _id: selectAllAppliedNumericalValuesIncludingErrorValues,
+  _id: null,
   fullName: null,
   email: null,
   address: null,
@@ -13,13 +14,38 @@ const initiaState = {
   user: userState,
   isError: false,
   isSuccess: false,
-  isLoadiing: false,
+  isLoading: false,
   message: "",
 };
+
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  try {
+    return authService.login(user);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
 
 export const authSlice = createSlice({
   name: "auth",
   initiaState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(login.fulfilled, (state) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.user = action.payload;
+    });
+    builder.addCase(login.rejected, (state) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.isSuccess = false;
+      state.user = null;
+    });
+  },
 });
+
+export default authSlice.reducer;
